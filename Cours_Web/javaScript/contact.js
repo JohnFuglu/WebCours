80908d8eb9760e30fbca72b15ff4eac05faa15f1
JohnFuglu <kuntz.romain@gmail.com>
2023-02-17 10:41:10 +0100

    val

--- Cours_Web/javaScript/contact.js
+++ Cours_Web/javaScript/contact.js
@@ -1,7 +1,1 @@
-const { Color } = require("three");
-
-const btn = document.getElementById("couleurBouton");
-btn.addEventListener('click',() =>{
-    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-    document.getElementById("labelContacts");
-})
+s
